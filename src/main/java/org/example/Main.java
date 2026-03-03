e546bb25001c6272ccea59bdbd438216760e1d3f
Timofey Tikhonov <ttkhnvv@gmail.com>
2026-03-03 18:22:36 +0500

    upd: task2 solved

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        runTask1();
+//        runTask1();
+        runTask2();
     }
 
     private static void runTask1() {
@@ -15,10 +16,31 @@ public class Main {
                 return;
             }
 
-            var n = scanner.nextInt();
+            int n = scanner.nextInt();
             var task = new Task1(n);
             var result = task.findAbsMin();
             System.out.println("[Task1]: Минимальное по модулю число: " + result);
+        }
+    }
+
+    private static void runTask2() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("[Task2]: Введите размер списка: ");
+            if (!scanner.hasNextInt()) {
+                System.out.println("[Task2]: Ошибка: ожидалось целое число.");
+                return;
+            }
+
+            int n = scanner.nextInt();
+            var task = new Task2(n);
+
+            System.out.println("[Task2]: До сортировки:");
+            task.print();
+
+            task.sort();
+
+            System.out.println("[Task2]: После сортировки:");
+            task.print();
         }
     }
 }
