aae962c0076efbcb14ab36f854c337f08dcfb4e1
Timofey Tikhonov <ttkhnvv@gmail.com>
2026-03-03 18:12:38 +0500

    upd: new fill method in RandomUtil

--- src/main/java/org/example/RandomUtil.java
+++ src/main/java/org/example/RandomUtil.java
@@ -1,12 +1,18 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public final class RandomUtil {
     private static final Random random = new Random();
 
     public static void fill(int[] nums) {
-        for(int i = 0; i < nums.length; i++)
+        for (int i = 0; i < nums.length; i++)
             nums[i] = random.nextInt(-1000, 1000);
+    }
+
+    public static void fill(ArrayList<Double> nums) {
+        for (int i = 0; i < nums.size(); i++)
+            nums.set(i, random.nextDouble(-1000, 1000));
     }
 }
